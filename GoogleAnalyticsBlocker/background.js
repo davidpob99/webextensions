a9962e5bdc56c8b927845f3f6f9b3181381660c4
davidpob99 <davidpob99@hotmail.com>
2018-01-30 18:31:20 +0100

    GoogleAnalyticsBlocker 1.1.0

--- GoogleAnalyticsBlocker/background.js
+++ GoogleAnalyticsBlocker/background.js
@@ -1,11 +1,46 @@
-  var pattern = ["*://www.google-analytics.com/*", "*://ssl.google-analytics.com/*"];
-  function block(requestDetails) {
-    // console.log("Blocking Google Analytics");
-    return {cancel: true};
-  }
+/*
+    GoogleAnalyticsBlocker
+    @author David Población Criado
+    @version 1.1.0 30/01/2018
+*/
 
-  browser.webRequest.onBeforeRequest.addListener(
-    block,
-    {urls: pattern},
-    ["blocking"]
-  );
+var pattern = ["*://www.google-analytics.com/*", "*://ssl.google-analytics.com/*"];
+var activate;
+window.addEventListener("load",function(){    
+   	activate = true;
+	browser.webRequest.onBeforeRequest.addListener(
+		block,
+		{urls: pattern},
+		["blocking"]
+	);
+});
+function block(requestDetails) {
+	// console.log("Blocking Google Analytics");
+	return {cancel: true};
+}
+
+browser.browserAction.onClicked.addListener(clicked);
+function clicked() {
+	if(activate){
+		// If the blocker was activated
+		browser.browserAction.setIcon({path:"icons/iconoff.svg"});
+		// console.log("Not blocking");
+		activate = false;
+		browser.webRequest.onBeforeRequest.removeListener(
+			block,
+			{urls: pattern},
+			["blocking"]
+		);
+	} else {
+		// If the blocker was not activated
+		browser.browserAction.setIcon({path:"icons/icon.svg"});
+		// console.log("Blocking");
+		activate = true;
+		browser.webRequest.onBeforeRequest.addListener(
+			block,
+			{urls: pattern},
+			["blocking"]
+		);
+	}
+}
+
